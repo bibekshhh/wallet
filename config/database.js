9e6a3e1dd76f111932a23986c33607a907eb9934
Yaman Sarabariya <63846784+yaman1337@users.noreply.github.com>
2022-10-28 11:46:03 +0545

    return application error on undefined mongo uri

--- config/database.js
+++ config/database.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const { MONGO_URI } = process.env;
 
+if(!MONGO_URI) {
+    console.log("Mongo uri is undefined or not found. Application shutting down")
+    process.exit(1)
+    return
+}
+
 exports.connect = () => {
     mongoose
         .connect(MONGO_URI, {
